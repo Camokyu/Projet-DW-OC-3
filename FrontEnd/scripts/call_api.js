120d1d73ad45e41a52c6d790393c237ae6a73f27
Camokyu <maellysvillard@gmail.com>
2023-07-17 21:20:32 +0200

    Suite refonte gestion et affichage travaux

--- FrontEnd/scripts/call_api.js
+++ FrontEnd/scripts/call_api.js
@@ -1,108 +1,113 @@
-let WORKS;
 const isConnected = checkToken();
 console.log("🚀 ~ file: call_api.js:2 ~ isConnected:", isConnected);
 
-getData("http://localhost:5678/api/categories")
-     .then((dataCategories) => {
-       dataCategories.forEach(createCategoryButton)
-     })        
-getWorks().then((data) => {
- WORKS = data; 
- data.forEach(createWorkElement)
-})
-
-getData("http://localhost:5678/api/works").then((data) => {
-  if (!isConnected) {
-    getData("http://localhost:5678/api/categories")
-      .then((dataCategories) => {
-        let worksArray;
-        worksArray = data;
-        console.log("worksArray", worksArray);
-        const buttonContainer = document.getElementById("button_container");
-
-        const figureContainer = document.getElementById("gallery");
-
-        const buttons = dataCategories.map((category, index) => {
-          const button = document.createElement("button");
-
-          button.textContent = category.name;
-
-          button.addEventListener("click", () => {
-            const filteredImages = worksArray.filter(
-              (work) => work.category.name == category.name
-            );
-            console.log(filteredImages);
-
-            setActiveButton(index);
-
-            figureContainer.innerHTML = "";
-
-            filteredImages.forEach((item) => {
-              let title = item.title;
-              let imageUrl = item.imageUrl;
-
-              let figure = document.createElement("figure");
-
-              let img = document.createElement("img");
-              img.src = imageUrl;
-              img.alt = title;
-
-              let figcaption = document.createElement("figcaption");
-              figcaption.textContent = title;
-
-              figure.appendChild(img);
-
-              figure.appendChild(figcaption);
-
-              let targetHTML = document.getElementById("gallery");
-
-              targetHTML.appendChild(figure);
-            });
-          });
-
-          buttonContainer.appendChild(button);
-
-          return button;
-        });
-
-        const buttonTous = document.createElement("button");
-
-        buttonTous.textContent = "Tous";
-
-        buttonTous.addEventListener("click", () => {
-          displayWorks("gallery");
-
-          figureContainer.innerHTML = "";
-          setActiveButton(-1);
-        });
-
-        const firstButton = buttonContainer.firstChild;
-        buttonContainer.insertBefore(buttonTous, firstButton);
-
-        worksArray = displayWorks("gallery");
-        console.log("travaux :", worksArray);
-
-        function setActiveButton(activeIndex) {
-          buttons.forEach((button, index) => {
-            if (index === activeIndex) {
-              button.classList.add("active");
-            } else {
-              button.classList.remove("active");
-            }
-          });
-          if (activeIndex === -1) {
-            buttonTous.classList.add("active");
-          } else {
-            buttonTous.classList.remove("active");
-          }
-        }
-      })
-      .catch((error) => {
-        console.error("Error:", error);
-      });
-  } else {
-    const worksArray = displayWorks("gallery");
-    console.log("travaux :", worksArray);
-    //gestion logout, boutons Modifier, bandeau mode édition en haut
-  }
+onLaunch().then(({ categories, works }) => {
+  console.log("result", categories);
+  console.log("result 2", works);
+  works.forEach(work => {
+    createWorkElement(work)
+  });
+  categories.unshift({ id: 0, name: "Tous" })
+  console.log("result after", categories);
+  //display des boutons (ajouter dans cette fonction de display un eventListener qui récupère l'ID du bouton et appeler une fonction qui filtre- cette partie 
+  //filtre est une fonction filter qui sera initialisée plus bas et qui prendra en paramètres l'ID du bouton cliqué et la liste des travaux aka allWorks, sont résultat doit
+  //être stocké car utilisé par display au-dessus, note : si ID = 0 alors display allWorks et si ID != 0 alors filter- filter donne un nouveau tableau de ce qui doit être affiché
+  // et on reprend donc les lignes 7 à 9 APRES avoir vidé le contenu déjà présent dans la gallery via une fonction ci-dessous :
+  let targetHTML = document.getElementById("gallery"); targetHTML.innerHTML = ""; //à couper-coller en première ligne dans la fonction filter
 });
+
+// getData("http://localhost:5678/api/works").then((data) => {
+//   if (!isConnected) {
+//     getData("http://localhost:5678/api/categories")
+//       .then((dataCategories) => {
+//         let worksArray;
+//         worksArray = data;
+//         console.log("worksArray", worksArray);
+//         const buttonContainer = document.getElementById("button_container");
+
+//         const figureContainer = document.getElementById("gallery");
+
+//         const buttons = dataCategories.map((category, index) => {
+//           const button = document.createElement("button");
+
+//           button.textContent = category.name;
+
+//           button.addEventListener("click", () => {
+//             const filteredImages = worksArray.filter(
+//               (work) => work.category.name == category.name
+//             );
+//             console.log(filteredImages);
+
+//             setActiveButton(index);
+
+//             figureContainer.innerHTML = "";
+
+//             filteredImages.forEach((item) => {
+//               let title = item.title;
+//               let imageUrl = item.imageUrl;
+
+//               let figure = document.createElement("figure");
+
+//               let img = document.createElement("img");
+//               img.src = imageUrl;
+//               img.alt = title;
+
+//               let figcaption = document.createElement("figcaption");
+//               figcaption.textContent = title;
+
+//               figure.appendChild(img);
+
+//               figure.appendChild(figcaption);
+
+//               let targetHTML = document.getElementById("gallery");
+
+//               targetHTML.appendChild(figure);
+//             });
+//           });
+
+//           buttonContainer.appendChild(button);
+
+//           return button;
+//         });
+
+//         const buttonTous = document.createElement("button");
+
+//         buttonTous.textContent = "Tous";
+
+//         buttonTous.addEventListener("click", () => {
+//           displayWorks("gallery");
+
+//           figureContainer.innerHTML = "";
+//           setActiveButton(-1);
+//         });
+
+//         const firstButton = buttonContainer.firstChild;
+//         buttonContainer.insertBefore(buttonTous, firstButton);
+
+//         worksArray = displayWorks("gallery");
+//         console.log("travaux :", worksArray);
+
+//         function setActiveButton(activeIndex) {
+//           buttons.forEach((button, index) => {
+//             if (index === activeIndex) {
+//               button.classList.add("active");
+//             } else {
+//               button.classList.remove("active");
+//             }
+//           });
+//           if (activeIndex === -1) {
+//             buttonTous.classList.add("active");
+//           } else {
+//             buttonTous.classList.remove("active");
+//           }
+//         }
+//       })
+//       .catch((error) => {
+//         console.error("Error:", error);
+//       });
+//   } else {
+//     const worksArray = displayWorks("gallery");
+//     console.log("travaux :", worksArray);
+//     //gestion logout, boutons Modifier, bandeau mode édition en haut
+//   }
+// });
--- FrontEnd/scripts/utils.js
+++ FrontEnd/scripts/utils.js
@@ -1,29 +1,29 @@
-function displayWorks(target) {
-  getWorks().then((data) => {
-    data.forEach((item) => {
-      const title = item.title;
-      const imageUrl = item.imageUrl;
+// function displayWorks(target) {
+//   getWorks().then((data) => {
+//     data.forEach((item) => {
+//       const title = item.title;
+//       const imageUrl = item.imageUrl;
 
-      const figure = document.createElement("figure");
+//       const figure = document.createElement("figure");
 
-      const img = document.createElement("img");
-      img.src = imageUrl;
-      img.alt = title;
+//       const img = document.createElement("img");
+//       img.src = imageUrl;
+//       img.alt = title;
 
-      const figcaption = document.createElement("figcaption");
-      figcaption.textContent = title;
+//       const figcaption = document.createElement("figcaption");
+//       figcaption.textContent = title;
 
-      figure.appendChild(img);
+//       figure.appendChild(img);
 
-      figure.appendChild(figcaption);
+//       figure.appendChild(figcaption);
 
-      const targetHTML = document.getElementById(target);
+//       const targetHTML = document.getElementById(target);
 
-      targetHTML.appendChild(figure);
-    });
-    return data;
-  });
-}
+//       targetHTML.appendChild(figure);
+//     });
+//     return data;
+//   });
+// }
 
 const getData = async (url) => {
   const response = await (await fetch(url)).json();
@@ -33,9 +33,10 @@ const getData = async (url) => {
 
 //méthode init qui initialise le site, donc doit récupérer categories et works
 
-function onLaunch() {
-  getCategories();
-  getWorks();
+async function onLaunch() {
+  const categories = await getCategories();
+  const works = await getWorks();
+  return { categories, works };
 }
 
 function getWorks() {
@@ -46,16 +47,16 @@ function getCategories() {
   return getData("http://localhost:5678/api/categories");
 }
 
+
 function createCategoryButton(category) {
   const buttonContainer = document.getElementById("button_container");
-    const button = document.createElement("button");
+  const button = document.createElement("button");
 
-    button.textContent = category.name;
+  button.textContent = category.name;
 }
 
 function createWorkElement(work) {
-  let title = item.title;
-  let imageUrl = item.imageUrl;
+  let{title, imageUrl} = work;
 
   let figure = document.createElement("figure");
 
@@ -74,3 +75,5 @@ function createWorkElement(work) {
 
   targetHTML.appendChild(figure);
 }
+
+// À faire : créer une méthode de display pour les boutons
